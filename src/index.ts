3b153e8ec1e2a38380c4c9331c4a3b4f0ddec9d3
Shinya Sakae <ss49919201@gmail.com>
2025-10-29 22:42:31 +0900

    Error API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { Hono } from 'hono';
-import { Context } from 'hono';
+import { Context, Hono } from 'hono';
 
 const app = new Hono<{ Bindings: Env }>();
+
+app.get('/err', async (c: Context) => {
+	c.status(500);
+	return c.text('Internal error');
+});
 
 app.get('/', async (c: Context) => {
 	await fetch('https://ss49919201.work/');
